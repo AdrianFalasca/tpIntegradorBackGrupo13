762c5e79061c6dbc1ef488c8bcfdbfbeb15f04fd
Adrián <adrifalasca@hotmail.com>
2025-07-06 20:33:24 -0300

    Primer commit

---
+++ tpIntegradorBackGrupo13/src/api/controllers/product.controllers.js
@@ -0,0 +1,160 @@
+// Importando modelos
+import Products from "../models/product.models.js";
+
+
+// Get all products
+export const getAllProducts = async (req, res) => {
+
+    try { // Optimizacion 1, manejo de errores con try/catch
+
+        let [rows] = await Products.selectAllProducts();
+    
+        // Optimizacion 2, responder con exito, aunque no existiera ningun producto
+        res.status(200).json({
+            payload: rows,
+            message: rows.length === 0 ? "No se encontraron productos" : "Productos encontrados"
+        });
+
+    } catch (error) {
+        
+        console.error("Error obteniendo productos", error);
+
+        res.status(500).json({
+            
+            error: "Error interno del servidor al obtener productos"
+            
+        })
+    }    
+}
+
+
+
+// Get product by id
+export const getProductById = async (req, res) => {
+    try {
+    
+        let { id } = req.params;
+
+        const [rows] = await Products.selectProductFromId(id);
+
+        // Verificamos si se encontro el producto
+        if(rows.length === 0) {
+            return res.status(404).json({
+                error: `No se encontro el producto con id: ${id}`
+            });
+        }
+
+        res.status(200).json({
+            payload: rows
+        })
+
+    } catch(error) {
+        console.error(`Error obteniendo producto con id ${id}`, error.message);
+
+        res.status(500).json({
+            error: "Error interno al obtener un producto por id"
+        });
+    }
+}
+
+
+// Create new product
+export const createProduct = async (req, res) => {
+
+    try {
+
+        // Recogemos y guardamos en variables los valores que recibimos del cliente
+        let { category, image, name, price } = req.body;
+
+
+        if(!category || !image || !name || !price) {
+            return res.status(400).json({
+                message: "Datos invalidos. Asegurate de enviar categoria, imagen, nombre y precio"
+            });
+        }
+
+
+        const [rows] = await Products.insertNewProduct(category, image, name, price);
+
+        // Devolvemos informacion util del insert para devolver el ID del producto creado
+        res.status(201).json({
+            message: "Producto creado con exito",
+            productId: rows.insertId
+        });
+
+
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            message: "Error interno del servidor",
+            error: error.message
+        });
+    }
+}
+
+
+// Modify product
+export const modifyProduct = async (req, res) => {
+    try {
+        let { id, category, image, name, price } = req.body;
+
+        // Validacion basica para comprobar que estan todos los campos
+        if(!id || !category || !image || !name || !price) {
+            return res.status(400).json({
+                message: "Faltan campos requeridos"
+            });
+        }
+
+
+        const [result] = await Products.updateProduct(id, category, image, name, price);
+
+        res.status(200).json({
+            message: "Producto actualizado correctamente"
+        });
+
+    } catch (error) {
+        console.error("Error al actualizar el producto", error);
+
+        res.status(500).json({
+            message: "Error interno del servidor",
+            error: error.message
+        });
+    }
+}
+
+
+
+// Remove product
+export const removeProduct = async (req, res) =>{
+    try {
+        let { id } = req.params;
+
+        if(!id) {
+            return res.status(400).json({
+                message: "Se requiere un id para eliminar un producto"
+            })
+        }
+
+
+        const [result] = await Products.deleteProduct(id);
+
+        if(result.affectedRows === 0) {
+            return res.status(404).json({
+                message: `No se encontro un producto con id ${id}`
+            });
+        }
+
+        return res.status(200).json({
+            message: `Producto con id ${id} eliminado correctamente`
+        });
+
+    } catch (error) {
+        console.error("Error en DELETE /products/:id", error);
+
+        res.status(500).json({
+            message: `Error al eliminar producto con id ${id}`, error,
+            error: error.message
+        });
+    }
+}
---
+++ tpIntegradorBackGrupo13/src/public/js/consultar.js
@@ -0,0 +1,80 @@
+const url = "http://localhost:3000/api";
+let getProduct_form = document.getElementById("getProduct-form");
+let getId_lista = document.getElementById("getId-list");
+
+getProduct_form.addEventListener("submit", async (event) => {
+
+    event.preventDefault(); // Evitamos el envio por defecto del formulario
+
+    try {
+
+        ///////////////////
+        // Optimizacion 1: Mostrar el estado de carga
+        getId_lista.innerHTML = "<p>Cargando usuario...</p>";
+
+
+        // Queremos extraer la informacion de los campos del formulario!
+        // Este es un objeto JavaScript especifico de informacion de formularios HTML
+        let formData = new FormData(event.target); 
+
+        // Transformamos el objeto FormData en un objeto JS normal
+        let data = Object.fromEntries(formData.entries());
+        console.log(data);
+
+        // Ahora que obtenemos el objeto con el campo de idProd, vamos a guardarlo en una variable
+        
+
+        ///////////////////
+        // Optimizacion 2: Eliminamos posibles espacios con .trim()
+        let idProd = data.idProd.trim(); 
+        console.log(idProd);
+
+
+        ///////////////////
+        // Optimizacion 3: Validacion basica
+        if(!idProd) {
+            throw new Error(`Error en el envio de datos del formulario`);
+        }
+
+        let response = await fetch(`${url}/products/${idProd}`);
+
+        ///////////////////
+        // Optimizacion 4: Manejamos el error en una posible respuesta no existosa
+        if(!response.ok) {
+            throw new Error(`Error ${response.status}: ${response.statusText}`); // Error 404: Not Found
+        }
+
+
+        let datos = await response.json();
+        console.log(datos);
+
+
+        ///////////////////
+        // Optimizacion 5: Verificamos si existen productos en la respuesta
+        if(!datos.payload || datos.payload.length === 0) {
+            throw new Error("No se encontro el producto solicitado")
+        }
+        
+
+
+        // Guardamos nuestro producto en una variable
+        let producto = datos.payload[0];
+        console.log(producto); 
+
+        let htmlProducto = `
+            <li id="producto-encontrado" class="li-listados productos-listados">
+                <img src="${producto.image}" alt="${producto.name}" class="img-listados">
+                <p>Id: ${producto.id} / Nombre: ${producto.name} / <strong>Precio: ${producto.price}$</strong></p>
+            </li>
+        `;
+
+        getId_lista.innerHTML = htmlProducto;
+
+    } catch (error) {
+        console.error("Error al obtener el producto: ", error);
+
+        ///////////////////
+        // Optimizacion 6: Escribir un mensaje de error
+        getId_lista.innerHTML = `<p>${error.message}</p>`
+    }
+})
